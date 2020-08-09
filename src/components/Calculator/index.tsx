d94638df108a399bcb990f3a00ce87d5916f66ef
Fernando Nagase <nagase.fernando@gmail.com>
2020-08-09 20:35:47 -0300

    feat: turn input group into a component

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 import './styles.css';
+import Input from '../Input';
 
 function Calculator() {
     return (
         <main>
             <form>
                 <div className="user-input-container">
-                    <div className="input-group">
-                        <label htmlFor="address">IP Address</label>
-                        <input type="text" name="address" placeholder="Ex: 192.168.4.4" id="address" />
-                    </div>
+                    <Input name="address" label="IP Address" placeholder="Ex: 192.168.4.4" />
                     <div className="radio-group">
                         <label>Addressing Type</label>
                         <div className="radio-inline">
@@ -24,44 +22,19 @@ function Calculator() {
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <div>
-                            <div className="input-group">
-                                <label htmlFor="mask">Subnet Mask</label>
-                                <input type="text" name="mask" placeholder="Ex: 255.255.255.0" id="mask" />
-                            </div>
-                        </div>
-                    </div>
+                    <Input name="mask" label="Subnet Mask" placeholder="Ex: 255.255.255.0" />
                     <button type="submit" className="button button-block">Calcular</button>
                 </div>
                 <div>
-                    <div className="input-group">
-                        <label htmlFor="range">Address Range</label>
-                        <input type="text" name="range" placeholder="Ex: 192.168.4.0 - 192.168.4.255" id="range" disabled />
+                    <Input name="range" label="Address Range" placeholder="Ex: 192.168.4.0 - 192.168.4.255" />
+                    <div className="input-row">
+                        <Input name="maxsubnets" label="Max Subnets" type="number" />
+                        <Input name="maxaddresses" label="Max Addresses" type="number" />
+                        <Input name="maxhosts" label="Max Hosts" type="number" />
                     </div>
                     <div className="input-row">
-                        <div className="input-group">
-                            <label htmlFor="maxsubnets">Max subnets</label>
-                            <input type="number"name="maxsubnets" id="maxsubnets" disabled />
-                        </div>
-                        <div className="input-group">
-                            <label htmlFor="maxaddresses">Max addresses</label>
-                            <input type="number" name="maxaddresses" id="maxaddresses" disabled />
-                        </div>
-                        <div className="input-group">
-                            <label htmlFor="maxhosts">Max hosts</label>
-                            <input type="number" name="maxhosts" id="maxhosts" disabled />
-                        </div>
-                    </div>
-                    <div className="input-row">
-                        <div className="input-group">
-                            <label htmlFor="subnet">Subnet address</label>
-                            <input type="text" name="subnet" placeholder="Ex: 192.168.4.0" id="subnet" disabled />
-                        </div>
-                        <div className="input-group">
-                            <label htmlFor="broadcast">Broadcast address</label>
-                            <input type="text" name="broadcast" placeholder="Ex: 192.168.4.0" id="broadcast" disabled />
-                        </div>
+                        <Input name="subnet" label="Subnet Address" placeholder="Ex: 192.168.4.0" />
+                        <Input name="broadcast" label="Subnet Address" placeholder="Ex: 192.168.4.255" />
                     </div>
                 </div>
             </form>
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,19 @@
+import React, { InputHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    name: string;
+    label: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, label, ...props }) => {
+    return (
+        <div className="input-group">
+            <label htmlFor={ name }>{ label }</label>
+            <input type="text" name={ name } id={ name } {...props} />
+        </div>
+    );
+}
+
+export default Input;
